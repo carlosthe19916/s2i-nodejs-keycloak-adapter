103b354c41597992c60ec2e36089515f9c4f3b00
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-06-01 18:48:26 -0500

    add env variable

--- current/keycloak-entrypoint.sh
+++ current/keycloak-entrypoint.sh
@@ -8,6 +8,20 @@ then
 
   echo "trying to create cli"
   /keycloak/bin/kcreg.sh create -s clientId=$KEYCLOAK_RESOURCE -s protocol=openid-connect -s rootUrl=/$KEYCLOAK_RESOURCE
+
+  if [ -n "$KEYCLOAK_WEB_ORIGIN" ]
+  then
+    export KEYCLOAK_WEB_ORIGIN=${KEYCLOAK_WEB_ORIGIN%/*}
+
+    echo "trying to update cli"
+    /keycloak/bin/kcreg.sh create -s clientId=$KEYCLOAK_RESOURCE -s protocol=openid-connect -s rootUrl=/$KEYCLOAK_RESOURCE
+    
+    echo "adding redirect uris" 
+    /keycloak/bin/kcreg.sh update $KEYCLOAK_RESOURCE -s 'redirectUris=["$KEYCLOAK_WEB_ORIGIN/*"]'
+
+    echo "adding web origins"
+    /keycloak/bin/kcreg.sh update $KEYCLOAK_RESOURCE -s 'webOrigins=["$KEYCLOAK_WEB_ORIGIN"]'
+  fi
 fi
 
 exec "$@"
