aab3490d556cfbbcfcab72b03969798e74781103
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-06-03 12:41:42 -0500

    resolve bug

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM carlosthe19916/centos7-s2i-angular-cli
 
 # Keycloak Client
-ADD kcreg.sh /opt/app-root/etc/keycloak
+ADD kcreg.sh /opt/app-root/etc/keycloak/bin
 RUN curl -L -o /opt/app-root/etc/keycloak/bin/client/keycloak-client-registration-cli.jar http://central.maven.org/maven2/org/keycloak/keycloak-client-registration-cli/3.0.0.Final/keycloak-client-registration-cli-3.0.0.Final.jar
 
 ADD keycloak-entrypoint.sh /opt/app-root/etc/keycloak
--- keycloak-entrypoint.sh
+++ keycloak-entrypoint.sh
@@ -4,23 +4,23 @@
 if [ -n "$KEYCLOAK_INITIAL_ACCESS_TOKEN" ] && [ -n "$KEYCLOAK_AUTH_SERVER_URL" ] && [ -n "$KEYCLOAK_REALM" ] && [ -n "$KEYCLOAK_RESOURCE" ]
 then
   echo "trying to configure keycloak-cli"
-  /keycloak/bin/kcreg.sh config initial-token $KEYCLOAK_INITIAL_ACCESS_TOKEN --server $KEYCLOAK_AUTH_SERVER_URL --realm $KEYCLOAK_REALM
+  /opt/app-root/etc/keycloak/bin/kcreg.sh config initial-token $KEYCLOAK_INITIAL_ACCESS_TOKEN --server $KEYCLOAK_AUTH_SERVER_URL --realm $KEYCLOAK_REALM
 
   echo "trying to create cli"
-  /keycloak/bin/kcreg.sh create -s clientId=$KEYCLOAK_RESOURCE -s protocol=openid-connect -s rootUrl=/$KEYCLOAK_RESOURCE
+  /opt/app-root/etc/keycloak/bin/kcreg.sh create -s clientId=$KEYCLOAK_RESOURCE -s protocol=openid-connect -s rootUrl=/$KEYCLOAK_RESOURCE
 
   if [ -n "$KEYCLOAK_WEB_ORIGIN" ]
   then
     export KEYCLOAK_WEB_ORIGIN=${KEYCLOAK_WEB_ORIGIN%/*}
 
     echo "trying to update cli"
-    /keycloak/bin/kcreg.sh create -s clientId=$KEYCLOAK_RESOURCE -s protocol=openid-connect -s rootUrl=/$KEYCLOAK_RESOURCE
+    /opt/app-root/etc/keycloak/bin/kcreg.sh create -s clientId=$KEYCLOAK_RESOURCE -s protocol=openid-connect -s rootUrl=/$KEYCLOAK_RESOURCE
     
     echo "adding redirect uris" 
-    /keycloak/bin/kcreg.sh update $KEYCLOAK_RESOURCE -s 'redirectUris=["$KEYCLOAK_WEB_ORIGIN/*"]'
+    /opt/app-root/etc/keycloak/bin/kcreg.sh update $KEYCLOAK_RESOURCE -s 'redirectUris=["$KEYCLOAK_WEB_ORIGIN/*"]'
 
     echo "adding web origins"
-    /keycloak/bin/kcreg.sh update $KEYCLOAK_RESOURCE -s 'webOrigins=["$KEYCLOAK_WEB_ORIGIN"]'
+    /opt/app-root/etc/keycloak/bin/kcreg.sh update $KEYCLOAK_RESOURCE -s 'webOrigins=["$KEYCLOAK_WEB_ORIGIN"]'
   fi
 fi
 
